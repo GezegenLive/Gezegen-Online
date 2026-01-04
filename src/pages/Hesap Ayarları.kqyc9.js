a4e763338e98d98557a83dc4659925843de1eb3a
Gezegen Online <contact@gezegen.info>
2026-01-04 06:28:18 +0200

    Initialize master

---
+++ src/pages/Hesap Ayarları.kqyc9.js
@@ -0,0 +1,15 @@
+// Velo API Referansı: https://www.gezegen.info/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+	// Velo framework API'yi kullanarak JavaScript kodunuzu buraya yazın
+
+	// Merhaba Dünya mesajını yazdırın:
+	// console.log("Merhaba dünya!");
+
+	// Sayfa ögelerinde fonksiyonlar çağırın, örneğin:
+	// $w("#button1").label = "Buraya tıkla!";
+
+	// Kodunuzu çalıştırmak için Çalıştır ögesine ya da Önizle'ye tıklayın.
+
+});
